50fcbb4e1111ab337e8a474f8771d6eb6159bdbd
Deepak Raj Gupta <123266812+Deepakrajgupta1203@users.noreply.github.com>
2025-04-09 01:54:18 +0530

    Update single_number_136.cpp

--- Array/single_number_136.cpp
+++ Array/single_number_136.cpp
@@ -26,5 +26,44 @@ public:
 // Better approch -> mapping
 
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+       
+       unordered_map<int,int>umap;
+
+       for(int i=0;i<nums.size();i++){
+          umap[nums[i]]++;
+       }
+
+       for(auto &pair:umap){
+           if(pair.second==1){
+            return pair.first;
+           }
+       }
+
+       return -1;
+    }
+};
+
+
+//Optimal
+
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans=0;
+
+        for(int i=0;i<nums.size();i++){
+            ans=ans^nums[i];
+
+        }
+
+        return ans;
+       
+    }
+};
 
 
