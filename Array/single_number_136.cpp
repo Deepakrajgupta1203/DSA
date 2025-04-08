31ca49f89a7509d98ed7dbf0e1bacd2c85aea86c
Deepak Raj Gupta <123266812+Deepakrajgupta1203@users.noreply.github.com>
2025-04-08 23:48:44 +0530

    Create single_number_136.cpp

---
+++ Array/single_number_136.cpp
@@ -0,0 +1,30 @@
+// Brute force 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     
+         for(int i=0;i<nums.size();i++){
+             int num=nums[i];
+             int count=0;
+
+             for(int j=0;j<nums.size();j++){
+                  if(nums[j]==num){
+                    count++;  
+                  }
+             }
+             if(count==1){
+                return nums[i];
+             }
+
+         }
+
+         return -1;
+    }
+};
+
+
+// Better approch -> mapping
+
+
+
+
