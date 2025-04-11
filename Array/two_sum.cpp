4d686a22265c3dcb62ef19fd44616009abd6780a
Deepak Raj Gupta <123266812+Deepakrajgupta1203@users.noreply.github.com>
2025-04-11 23:30:48 +0530

    Update two_sum.cpp

--- Array/two_sum.cpp
+++ Array/two_sum.cpp
@@ -1,2 +1,22 @@
 
 
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+
+        unordered_map<int,int> umap;
+
+        for(int i=0;i<nums.size();i++){
+
+            int current=nums[i];
+            int req=target-current;
+
+            if(umap.find(req)!=umap.end()){
+                return {i,umap[req]};
+            }
+            umap[current]=i;
+        } 
+            return {-1,-1};       
+    }
+};
